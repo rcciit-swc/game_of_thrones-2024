2012ffb9bd8892ddb215ea5f6f7d41b3860122df
ADLM10 <de.alimpan5@gmail.com>
2023-11-28 22:50:28 +0530

    initialised colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ const config: Config = {
       fontFamily: {
         got: ["Got", "sans-serif"],
       },
+      colors:{
+        primary:"#F8714B",
+        secondary:"#2D3493",
+        body:"#1E1E1E",
+        neutral:"#FEFEFA"
+      }
     },
   },
   plugins: [],
