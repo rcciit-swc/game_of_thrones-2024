790506956e8ce6d812aadc5cf59a97f195b576a0
soumyarajbag <soumyarajbag@gmail.com>
2023-12-03 23:38:04 +0530

    Commit before merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import SponsorPage from "@/components/home/Sponsor";
 import Hero from "@/components/home/Hero";
 import TeachersDesk from '@/components/home/TeachersDesk';
 import About from "@/components/home/About";
+import HeroSection from "@/components/home/HeroSection";
 
 export default function Home() {
   return (
     <main className="">
-        <Hero />
+       <HeroSection />
         <About />
       <TeachersDesk />
       <SponsorPage />
---
+++ src/components/home/HeroSection.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <>
+     <div className=" flex flex-row justify-between ">
+
+    
+     </div>
+    </>
+  )
+}
+
+export default HeroSection
