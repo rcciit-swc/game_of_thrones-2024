34211dbdfd0d09efe7157b3eb930463884e096e3
Manice18 <manishmanice2003@gmail.com>
2023-12-03 10:30:15 +0530

    Layouts fixed, reponsiveness done(except hero)

---
+++ src/app/(home)/_components/TeachersDeck.tsx
@@ -0,0 +1,47 @@
+import { teachers } from "@/utils/constant";
+import Image from "next/image";
+import { SectionHeader } from "@/components";
+
+export default function TeachersDesk() {
+  return (
+    <section className="text-neutral">
+      <SectionHeader text="Principal's Deck" />
+
+      <div className="flex flex-col items-center justify-center gap-8 lg:flex-row">
+        {/* Mobile Image */}
+        <div className="relative block md:hidden">
+          <div className="absolute z-[-10] ml-[-10px] mt-[10px] h-[250px] w-[100px] rounded-3xl bg-primary" />
+          <Image
+            src="/teachers/principal.png"
+            className="h-72 w-60"
+            alt="Principal"
+            priority
+            width={92}
+            height={74}
+          />
+        </div>
+        <div className="relative mx-2 flex w-[340px] flex-col rounded-xl bg-primary md:w-[600px] lg:w-[800px]">
+          <div className="my-6 pl-8 text-2xl">Dear students,</div>
+          <p className="px-4 text-justify text-sm md:pl-12 md:pr-72 md:text-xl">
+            {teachers[0].speech}
+          </p>
+          <p className="mb-6 mt-4 pl-20 text-sm font-semibold tracking-wider md:pl-56 md:text-xl">
+            {teachers[0].signL1} <br /> {teachers[0].signL2}
+          </p>
+
+          {/* PC Image */}
+          <div className="absolute right-[-120px] top-[40%] mt-[-180px] hidden md:block">
+            <Image
+              alt="Principal's Image"
+              className="h-[390px] w-[300px] rounded-3xl"
+              height={390}
+              width={300}
+              src={`/${teachers[0].img}`}
+            />
+            <div className="ml-[-10px] mt-[-330px] h-[320px] w-[300px] rounded-3xl md:bg-black" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import { Hero, SponsorPage, TeachersDeck, About } from "./_components";
+
+export default function Home() {
+  return (
+    <main className="max-w-full space-y-[100px]">
+      <div className="flex flex-col">
+        <Hero />
+        <About />
+      </div>
+      <TeachersDeck />
+      <SponsorPage />
+    </main>
+  );
+}
