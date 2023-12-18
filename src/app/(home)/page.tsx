638b68f1b7ff53c3b18c9408210c47cf084f07bf
soumyarajbag <soumyarajbag@gmail.com>
2023-12-18 14:32:08 +0530

    fix:images

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,11 @@
-import {  SponsorPage, TeachersDeck, About, Events } from "./_components";
-import HeroSection from "./_components/HeroSection";
+import {  SponsorPage, TeachersDeck, About, Events, Hero } from "./_components";
+
 
 export default function Home() {
   return (
     <main className="max-w-full space-y-[100px]">
       <div className="flex flex-col">
-        <HeroSection />
+        <Hero />
 
         <About />
       </div>
