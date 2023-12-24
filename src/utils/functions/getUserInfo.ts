5971fd8d3ac68969061b408d10951861acd661a8
Manice18 <manishmanice2003@gmail.com>
2023-12-25 04:27:17 +0530

    fix: middleware functions

--- src/utils/functions/getUserInfo.ts
+++ src/utils/functions/getUserInfo.ts
@@ -1,18 +1,19 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export async function getUserInfo(){
+export async function getUserInfo() {
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
+  const { data, error } = await supabase.auth.getSession();
+  const userDetails = await supabase
+    .from("users")
+    .select()
+    .eq("id", data.session?.user.id);
 
-    const supabase = createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      );
-      const { data, error } = await supabase.auth.getSession();
-      const userDetails = await supabase.from('users').select().eq('id', data.session?.user.id);
-    
-    
-    if (error) {
-        console.error("Error getting user data:", error);
-        return;
-    }
-    return userDetails?.data?.[0];
+  if (error) {
+    console.error("Error getting user data:", error);
+    return;
+  }
+  return userDetails?.data?.[0];
 }
