651e02823aabae5b0020854c277ca68e20ba0b00
Sansrita <sansritasaha@gmail.com>
2023-12-28 01:14:44 +0530

    gallery_update

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -14,19 +14,25 @@ const Gallery = () => {
       <SectionHeader text="gallery" />
       <div className="mx-auto grid max-w-[1600px] grid-cols-1 items-center justify-center gap-20 rounded-2xl px-5 py-10 font-sans md:grid-cols-2 md:border md:px-10 md:py-20 lg:grid-cols-3">
         {gallery.map((photo, index) => (
-          <div key={index}>
+          <div className="relative w-[400] h-[300px] rounded-2xl overflow-hidden 
+          grayscale hover:grayscale-0
+        
+          " key={index}>
+
             <Image
               src={photo.image}
-              alt="football"
-              className="h-full w-full rounded-2xl object-cover "
-              width={480}
-              height={480}
-              sizes="(min-width: 1680px) 453px, (min-width: 1040px) calc(30.16vw - 48px), (min-width: 780px) calc(50vw - 81px), calc(100vw - 40px)"
+              alt="image"
+              fill={true}
+              style={{ objectFit: "cover" }}
+              // className="h-full w-full rounded-2xl object-cover "
+              // width={480}
+              // height={480}
+              // sizes="(min-width: 1680px) 453px, (min-width: 1040px) calc(30.16vw - 48px), (min-width: 780px) calc(50vw - 81px), calc(100vw - 40px)"
             />
-            <h1 className="text-center text-2xl font-semibold tracking-widest">
+            {/* <h1 className="text-center text-2xl font-semibold tracking-widest">
               {photo.sport}
-            </h1>
-            <h1 className="text-center text-xl">{photo.title}</h1>
+            </h1> */}
+            {/* <h1 className="text-center text-xl">{photo.title}</h1> */}
           </div>
         ))}
       </div>
