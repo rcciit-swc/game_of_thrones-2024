f96f5abe6bb58f336ff17c30d1ec249f2ed07dcc
ADLM10 <de.alimpan5@gmail.com>
2024-01-01 15:21:52 +0530

    remove:unused functions

--- src/utils/functions/logout.ts
+++ src/utils/functions/logout.ts
@@ -1,9 +1,5 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { supabase } from "@/lib/supabase-client";
 
 export async function logOut() {
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
   await supabase.auth.signOut();
 }
