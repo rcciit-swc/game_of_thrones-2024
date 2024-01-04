66fd3aa5616e4fadaa928b38032a8471178d8f01
Sansrita <sansritasaha@gmail.com>
2024-01-05 00:49:31 +0530

    team

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -15,7 +15,7 @@ const TeamsPage = () => {
       <SectionHeader text="Our Team" />
       <div className="mx-auto grid max-w-[1600px] grid-cols-1 items-center justify-center gap-20 rounded-2xl px-5 py-10 font-sans md:grid-cols-2 md:border md:px-10 md:py-20 lg:grid-cols-3">
         {team.map((photo, index) => (
-          <div className="relative w-[400] h-[300px] rounded-2xl overflow-hidden 
+          <div className="relative w-[400] h-[600px] rounded-2xl overflow-hidden 
           grayscale hover:grayscale-0" key={index}>
 
             <Image
@@ -28,10 +28,10 @@ const TeamsPage = () => {
               // height={480}
               // sizes="(min-width: 1680px) 453px, (min-width: 1040px) calc(30.16vw - 48px), (min-width: 780px) calc(50vw - 81px), calc(100vw - 40px)"
             />
-            {/* <h1 className="text-center text-2xl font-semibold tracking-widest">
+            <h1 className="text-center text-2xl font-semibold tracking-widest">
               {photo.sport}
-            </h1>  */}
-            {/* <h1 className="text-center text-xl">{photo.title}</h1>  */}
+            </h1>  
+            <h1 className="text-center text-xl">{photo.title}</h1>  
           </div>
         ))}
       </div>
