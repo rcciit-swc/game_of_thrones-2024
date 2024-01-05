93c471616f49c958b60f2260f17eaeeb442614e8
ADLM10 <de.alimpan5@gmail.com>
2024-01-05 05:32:42 +0530

    add:Download CSV

--- src/app/coordinator-dashboard/[event]/page.tsx
+++ src/app/coordinator-dashboard/[event]/page.tsx
@@ -1,14 +1,13 @@
 "use client";
-import { SectionHeader } from '@/components';
-import { events } from '@/utils';
-import { getRegisteredTeams } from '@/utils/functions/getRegisteredTeams';
-import { useParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import Table from '../_components/Table';
+import { SectionHeader } from "@/components";
+import { events } from "@/utils";
+import { getRegisteredTeams } from "@/utils/functions/getRegisteredTeams";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Table from "../_components/Table";
 
 const Page = () => {
   let params = useParams();
-
 
   const [registrationData, setRegistrationData] = useState<any[]>([]);
 
@@ -16,26 +15,57 @@ const Page = () => {
 
   useEffect(() => {
     const getRegistrations = async () => {
+      const eventType = events.find((event) => event.id === params.event)
+        ?.teamType;
+
+      let regData: any[] = [];
+
       const registrationData = await getRegisteredTeams({
         event_id_param: params.event as string,
       });
-      setRegistrationData(registrationData!);
+
+      registrationData?.map((team: any) => {
+        if (eventType === "Team") {
+          regData.push({
+            ...team,
+            teamType: "team",
+          });
+        } else if (eventType === "Singles & Doubles") {
+          if (team.is_team) {
+            regData.push({
+              ...team,
+              teamType: "Doubles",
+            });
+          } else {
+            regData.push({
+              ...team,
+              teamType: "Singles",
+            });
+          }
+        } else if (eventType === "Singles") {
+          regData.push({
+            ...team,
+            teamType: "Singles",
+          });
+        }
+      });
+
+      setRegistrationData(regData);
     };
 
     if (params.event) {
-        getRegistrations();
-    }
-    else{
-        return;
+      getRegistrations();
+    } else {
+      return;
     }
   }, [params.event]);
 
   return (
     <div className="flex flex-col pt-20">
-        <SectionHeader text={`Coordinator Dashboard - ${eventName}`} />
-        <Table registrationData={registrationData} />
+      <SectionHeader text={`Coordinator Dashboard - ${eventName}`} />
+      <Table registrationData={registrationData} />
     </div>
   );
 };
 
-export default Page
+export default Page;
--- src/app/coordinator-dashboard/_components/Table.tsx
+++ src/app/coordinator-dashboard/_components/Table.tsx
@@ -1,28 +1,69 @@
-import { events } from "@/utils";
-import { useState } from "react";
+import { CSVLink } from "react-csv";
 
 interface TableProps {
   registrationData: any[];
 }
 
+export const Headers = [
+  {
+    label: "Id",
+    key: "team_id",
+  },
+  {
+    label: "Team Name",
+    key: "team_name",
+  },
+  {
+    label: "Event Name",
+    key: "event_name",
+  },
+  {
+    label: "Team Type",
+    key: "teamType",
+  },
+  {
+    label: "Team Lead/Player Name",
+    key: "name",
+  },
+  {
+    label: "Team Lead/Player Phone",
+    key: "team_lead_phone",
+  },
+  {
+    label: "College",
+    key: "college",
+  },
+];
+
 const Table = ({ registrationData }: TableProps) => {
 
-  const getEventType = (event_id: string, is_team: boolean) => {
-    const event = events.find((event) => event.id === event_id);
-    if (is_team) {
-      return event?.teamType === "Team" ? "Team" : "Doubles";
-    } else {
-      return "Singles";
-    }
-  }
+  let DateTime = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
   return (
     <div className="overflow-x-auto px-3">
-      <h1 className="text-2xl font-bold">Registrations</h1>
+      <div className="mb-4 flex flex-row items-center justify-between px-3">
+        <h1 className="text-2xl font-bold">Registrations</h1>
+        <CSVLink
+          data={registrationData}
+          headers={Headers}
+          filename={`registrations-${DateTime}.csv`}
+          className="w-fit-content rounded-md bg-blue-500 px-4 py-2 text-white shadow-md hover:bg-blue-600"
+        >
+          Download CSV
+        </CSVLink>
+      </div>
       <table className="w-full table-auto">
         <thead>
           <tr>
-            <th className="px-4 py-2 text-left">Event Name</th>
+            <th className="px-4 py-2 text-left">Sl. No.</th>
             <th className="px-4 py-2 text-left">Type</th>
             <th className="px-4 py-2 text-left">Team Name</th>
             <th className="px-4 py-2 text-left">College</th>
@@ -31,10 +72,10 @@ const Table = ({ registrationData }: TableProps) => {
           </tr>
         </thead>
         <tbody>
-          {registrationData?.map((row) => (
+          {registrationData?.map((row, index) => (
             <tr key={row.id}>
-              <td className="border px-4 py-2">{row.event_name}</td>
-              <td className="border px-4 py-2">{getEventType(row.event_id,row.is_team)}</td>
+              <td className="border px-4 py-2">{index + 1}</td>
+              <td className="border px-4 py-2">{row.teamType}</td>
               <td className="border px-4 py-2">{row.team_name}</td>
               <td className="border px-4 py-2">{row.college}</td>
               <td className="border px-4 py-2">{row.name}</td>
